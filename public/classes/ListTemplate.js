fb654199a40e20e063b6d7b4182a6a91dd0dce4e
AmrDhaliwal <amritdhaliwalz@gmail.com>
2021-05-03 15:57:00 -0700

    Created classes for the project and was able to succesfully render the changes to the HTML.

---
+++ public/classes/ListTemplate.js
@@ -0,0 +1,22 @@
+// Registers a list container (ul) to the container
+// Creates a render method to render a new (li) to the container
+export class ListTemplate {
+    constructor(container) {
+        this.container = container;
+    }
+    render(item, heading, pos) {
+        const li = document.createElement('li');
+        const h4 = document.createElement('h4');
+        h4.innerText = heading;
+        li.append(h4);
+        const p = document.createElement('p');
+        p.innerText = item.format();
+        li.append(p);
+        if (pos === 'start') {
+            this.container.prepend(li);
+        }
+        else {
+            this.container.append(li);
+        }
+    }
+}
